d1232b4e4445a8da37349c9faeb3b87b76a9b67e
TRUDgame <gertrud.agger@gmail.com>
2020-09-23 22:48:54 +0200

    DatavisMeshTutz

--- VizData Unity project/Assets/Assignments/Meshes/Scripts/MeshScript.cs
+++ VizData Unity project/Assets/Assignments/Meshes/Scripts/MeshScript.cs
@@ -5,23 +5,50 @@ using UnityEngine;
 public class MeshScript : MonoBehaviour
 {
     public int xSize, ySize; 
+
+
     private Vector3[] vertices;
+    private Mesh mesh;
+
 
 
     private void Awake()
     {
-        Generate();
+        StartCoroutine(Generate());
     }
-    private void Generate()
+
+    private IEnumerator Generate()
     {
+        WaitForSeconds wait = new WaitForSeconds(0.05f);
+
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Grid";
+
         vertices = new Vector3[(xSize + 1) * (ySize + 1)];
         for (int i = 0, y = 0; y <= ySize; y++)
         {
             for (int x = 0; x <= xSize; x++, i++)
             {
                 vertices[i] = new Vector3(x, y);
+                
             }
         }
+
+        mesh.vertices = vertices;
+
+        int[] triangles = new int[xSize *6];
+        for (int ti = 0, vi = 0, x = 0; x < xSize; x++, ti += 6, vi++){
+            triangles[ti] = vi;
+            triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+            triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+            triangles[ti + 5] = vi + xSize + 2;
+            
+            mesh.triangles = triangles;
+            yield return wait;
+        }
+       
+
+        
     }
 
     private void OnDrawGizmos()
